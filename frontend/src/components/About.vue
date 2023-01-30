d494646f53d43189a62a44e2a47d2c6df9814acf
Dorijan Kos <dorijan.kos@epitech.eu>
2023-01-30 11:22:20 +0100

    deployed version

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -26,19 +26,19 @@ export default {
         {
           id: 1,
           name: "Dorijan Kos",
-          img: "../../public/Snapchat-37253659.jpg",
+          img: "/Snapchat-37253659.jpg",
           description: "Front-End developer",
         },
         {
           id: 2,
           name: "Aqsa Muzaffar Ahmed",
-          img: "../../public/MicrosoftTeams-image (1).png",
+          img: "/MicrosoftTeams-image (1).jpg",
           description: "Back-End developer",
         },
         {
           id: 3,
           name: "Vera Koliverda",
-          img: "../../public/MicrosoftTeams-image.png",
+          img: "/MicrosoftTeams-image.jpg",
           description: "A.I. developer",
         },
       ],
